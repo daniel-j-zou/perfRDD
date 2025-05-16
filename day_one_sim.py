9797887ebe910261239b13be35824cc54ae344fa
LukaT05 <lukat@umich.edu>
2025-05-16 11:20:21 -0400

    first run of making a dataset

---
+++ day_one_sim.py
@@ -0,0 +1,23 @@
+# Q = I_1 + gamma * Z + eta
+#  Noise and Z should be normal because we want Q to be normal as
+# well as for homoscedasticity and normality of G_bar
+# No trivial aspects of the simulation
+#
+# Create the dataset and runs the algorithm on the dataset
+# Make this first step a function that outputs interesting results (optimal threshold in non-performative)
+
+#libraries
+import numpy as np
+
+#Variables
+n = 10000
+z_bar = 0
+z_var = 1
+eta_var = 1
+gamma = 1
+
+z = np.random.normal(z_bar, z_var, n)
+i_1 = np.ones(n)
+eta = np.random.normal(0, eta_var, n)
+q = i_1 + gamma * z + eta
+print(q)
